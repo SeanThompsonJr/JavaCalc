2f3c01ce0a2f6bd2f230dd49b20a73ba2fc2fdb6
Leader-deapool <65671096+Leader-deapool@users.noreply.github.com>
2023-02-24 22:43:56 -0500

    Day1
    Need to learn alot of this

--- JavaCalculator/src/javacalculator/JavaCalculator.java
+++ JavaCalculator/src/javacalculator/JavaCalculator.java
@@ -3,21 +3,46 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package javacalculator;
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 /**
  *
- * @author deadp
+ * 
  */
-public class JavaCalculator {
 
-    /**
-     * @param args the command line arguments
-     */
+//interface -whatever that means
+public class JavaCalculator implements ActionListener{
+    
+    // all of this is from the jframe library
+    JFrame frame;
+    JTextField textfield;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[8];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton decButton, equButton, delButton, clrButton;
+    JPanel panel;
+    
+    //font object
+    Font myFont = new Font("Ink Free", Font.BOLD,30);
+    //variables
+    double num1 = 0,num2=0,result=0;
+    char operator;
+    
+    //class constructor
+    JavaCalculator() {
+        
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        //new object from the constructor
+        JavaCalculator calc = new JavaCalculator();
         
         
-        
+    }
+    //comes with ActionListener
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
