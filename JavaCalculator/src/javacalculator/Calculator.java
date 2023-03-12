b3f215dda504efb24c0ef53bc300e8b45943389b
Leader-deapool <65671096+Leader-deapool@users.noreply.github.com>
2023-03-12 17:59:33 -0400

    Day 3
    -"Delete" and "Clear" button drawn to the screen
    -made the "textfield" not editable

--- JavaCalculator/src/javacalculator/Calculator.java
+++ JavaCalculator/src/javacalculator/Calculator.java
@@ -40,8 +40,51 @@ public class Calculator implements ActionListener{
         textfield = new JTextField();
         textfield.setBounds(50,25,300,50);
         textfield.setFont(myFont);
+        textfield.setEditable(false); // makes the textfield not editable
+        
+        //creates buttons for operations
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Delete");
+        clrButton = new JButton("Clear");
+        
+        //puts the operation buttons in an array
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decButton;
+        functionButtons[5] = equButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+        
+        //loops through all operation buttons
+        for(int i = 0; i < 8 ; i++){
+            functionButtons[i].addActionListener(this); //adds an action listener to each button
+            functionButtons[i].setFont(myFont); //sets the buttons fonts
+            functionButtons[i].setFocusable(false); //removes highlight when hover
+        }
+        //loops through all number buttons
+        for(int i = 0; i < 10 ; i++){
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this); //adds an action listener to each button
+            numberButtons[i].setFont(myFont); //sets the buttons fonts
+            numberButtons[i].setFocusable(false); //removes highlight when hover
+        }
+        
+        //drawing them to a positition and giving them a size
+        delButton.setBounds(50,430,145,50);
+        clrButton.setBounds(205,430,145,50);
         
         
+        // draws them to the actual window
+        frame.add(delButton);
+        frame.add(clrButton);
+        //adds the textfield to the window
         frame.add(textfield);
         //makes the whole window visible
         frame.setVisible(true);
